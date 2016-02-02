c7ab7f3f7f85991d52e0ddeeace7926069a845d9
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-02 14:32:35 -0500

    setting up the document for the javascript file structure

---
+++ main.js
@@ -0,0 +1,7 @@
+//Loading the javascript when the document window is ready
+$(document).ready(function(){
+    //wrapping everything in an IIFE to keep the global scope clean
+    (function(){
+    
+    })();
+});
