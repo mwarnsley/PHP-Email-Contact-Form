9ec72054ea70b4dbfdc3692eb7c8ad673886f1ea
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-02 14:29:36 -0500

    Adding the main js file and adding jquery and mainjs file to the main php html page

--- index.php
+++ index.php
@@ -36,5 +36,8 @@
                 </div>
             </div>
         </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script type="text/javascript" src="main.js"></script>
     </body>
 </html>
