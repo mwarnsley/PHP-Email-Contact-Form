01eace98699ad9ca8c5f9fc9f7031683ddb3d8d6
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-02 14:53:04 -0500

    Fixing some css on the form itself along with adding the javascript to hide and show the placeholder on focus and on blue

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 $(document).ready(function(){
     //wrapping everything in an IIFE to keep the global scope clean
     (function(){
-    
+        //function to hide and show the placeholder on focus and blur of the input boxes
+        (function removePlaceholder(){
+            $("input").focus(function(){
+                //set the data of the placeholder to the placeholder itself and then remove it on focus
+               $(this).data("placeholder", $(this).attr("placeholder")).attr("placeholder", "");
+            }).blur(function(){
+                //set the data of the attribute back to itself on blur
+                $(this).attr("placeholder", $(this).data("placeholder"));
+            });
+        })();
+
     })();
 });
