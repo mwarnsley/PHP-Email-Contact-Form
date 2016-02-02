02066f41aab48304d9e76128f366cc14c3c0b061
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-02 13:52:12 -0500

    adding the html structure to the php page

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-ECHO is on.
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Email Contact Form</title>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="main.css" />
+    </head>
+    <body>
+        <div class="container"></div>
+    </body>
+</html>
