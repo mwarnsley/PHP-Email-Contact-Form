3311473d90c0b17b8708c217e61655fff3156909
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-02 14:26:46 -0500

    html and css editing of the email contact form using bootstrap and css

--- index.php
+++ index.php
@@ -20,12 +20,17 @@
             <div class="row">
                 <div class="col-md-6 col-md-offset-3">
                     <div id="form-container">
+                        <h1>Email Contact Form</h1>
+                        <p class="lead">Please get in touch - I'll get back to you as soon as possible. Thank You!</p>
                         <form method="post">
-                            <input type="text" name="fname" placeholder="First Name"><br>
-                            <input type="text" name="lname" placeholder="Last Name"><br>
-                            <input type="text" name="subject" placeholder="Subject"><br>
-                            <input type="text" name="email" placeholder="Email Address"><br>
-                            <input type="submit" name="submit" value="SEND">
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="fname" placeholder="First Name"><br>
+                                <input class="form-control" type="text" name="lname" placeholder="Last Name"><br>
+                                <input class="form-control" type="text" name="email" placeholder="Email Address"><br>
+                                <input class="form-control" type="text" name="subject" placeholder="Subject"><br>
+                                <textarea class="form-control" name="comment" placeholder="Comments"></textarea><br>
+                                <input class="btn btn-success btn-lg" type="submit" name="submit" value="SEND">
+                            </div>
                         </form>
                     </div>
                 </div>
